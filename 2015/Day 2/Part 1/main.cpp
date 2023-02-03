3471f766b096c03e199b72aff9c2597da4118e30
Kagitoshi <santocomet@hotmail.com>
2023-02-03 17:51:00 +0900

    Day 2. Started the formula. Need to add a parsing function.

---
+++ 2015/Day 2/Part 1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int areaCalculation(int a, int b)
+{
+    int area{a * b};
+    return area;
+}
+
+int areaComparison(int sideOne, int sideTwo, int sideThree)
+{
+    int temp{(sideOne < sideTwo) ? sideOne : sideTwo};
+
+    temp = (temp < sideThree) ? temp : sideThree;
+
+    return temp;
+}
+
+
+
+
+int main()
+{
+    int length{};
+    int width{};
+    int height{};
+
+
+    int sideOne{areaCalculation(length,width)};
+    int sideTwo{areaCalculation(width, height)};
+    int sideThree{areaCalculation(height, length)};
+
+    int smallestArea{areaComparison(sideOne,sideTwo, sideThree)};
+
+    std::cout << "The total amount of wrapping paper needed is " <<
+    (2*sideOne) + (2*sideTwo) + (2*sideThree) + smallestArea << "\n\n";
+
+
+    return 0;
+}
