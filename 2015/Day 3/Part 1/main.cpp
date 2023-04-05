c02e120967d3a7be1e66a9d68e7be32ff3fc8071
Kagitoshi <santocomet@hotmail.com>
2023-04-05 14:57:02 +0900

    Finished code for Day 3 - Part 1 - Code passed.

--- 2015/Day 3/Part 1/main.cpp
+++ 2015/Day 3/Part 1/main.cpp
@@ -1,22 +1,65 @@
 #include <iostream>
-#include <map>
 #include <string>
-#include <vector>
-
+#include <set>
+#include <fstream>
 
 int main()
 {
-    std::string directions{"^>v<"};
+    //Location of file with directions
+    std::ifstream fileName("../directions.txt");
 
-    std::vector<std::vector<int>> houses{};
+    //Coordinate variables
+    int x{0};
+    int y{0};
 
-    for(int i{0}; i < directions.size(); ++i)
+    //Set coordinate variables as an initializer.
+    std::set <std::pair<int, int>> houseCoordinates;
+
+    //Adding Santa's first house
+    houseCoordinates.insert(std::make_pair(x, y));
+
+    //Creating char variable to track direction read from the file.
+    char direction{};
+
+    while (fileName.get(direction))
     {
-        //Reading the string one character at a time.
-        if(directions[i])
-        std::cout << directions[i] << '\n';
+            //Plus one to the Y coordinate for North direction.
+            if(direction == '^')
+            {
+                y = ++y;
+            }
+            //Minus one to the Y coordinate for South direction.
+            if(direction == 'v')
+            {
+                y = --y;
+            }
+
+            //Plus one to the X coordinate for East direction.
+            if(direction == '>')
+            {
+                x = ++x;
+            }
+
+            //Minus one to the X coordinate for West direction.
+            if(direction == '<')
+            {
+                x = --x;
+            }
+
+            //Adding new coordinates to the set.
+            houseCoordinates.insert(std::make_pair(x, y));
+
     }
 
+
+// Below is to test if coordinates are printing correctly/
+//    for (const auto& i : houseCoordinates)
+//    {
+//        std::cout << '(' << i.first << ',' << i.second << ") \n \n";
+//    }
+
+    //Displaying the amount of houses Santa visited.
+    std::cout << "Santa dropped off " << houseCoordinates.size() << " presents, each to different houses.";
 
     return 0;
 }
