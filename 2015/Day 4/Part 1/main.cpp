a08125ecf261764893d28a5269e0e33ac9a3136a
Kagitoshi <santocomet@hotmail.com>
2023-04-06 13:53:43 +0900

    Day 4 - Part 1 - Completed.

---
+++ 2015/Day 4/Part 1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "md5.h"
+
+
+int main(int argc, char *argv[])
+{
+    //Entered my Puzzle Answer for the hex variable
+    std::string hex{"bgvyzdsv"};
+
+    //Creating a variable to find the answer.
+    int number{0};
+
+    //Creating an output variable to find the output that gives use 5 leading zeros.
+    std::string output{md5(hex + std::to_string(number))};
+
+    //Running a while loop that increases the number variable by 1 until the output has 5 leading zeros.
+    while (output.substr(0,5) != "00000")
+    {
+        number++;
+        output = md5(hex + std::to_string(number));
+    }
+
+    //Displaying the answer.
+    std::cout << "md5 of " << hex + std::to_string(number) << ": " << md5(hex + std::to_string(number)) << '\n';
+
+    return 0;
+}
